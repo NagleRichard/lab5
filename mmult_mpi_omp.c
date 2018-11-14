e8c62bb7ae8620d6b23a0091eecbe65c4850e2cf
NagleRichard <43142317+NagleRichard@users.noreply.github.com>
2018-11-13 23:49:58 -0500

    Update mmult_mpi_omp.c

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -11,19 +11,6 @@ void compare_matrix(double *a, double *b, int nRows, int nCols);
 
 /** 
     Program to multiply a matrix times a matrix using both
-    mpi to distribute the computat#include "mpi.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/times.h>
-#define min(x, y) ((x)<(y)?(x):(y))
-
-double* gen_matrix(int n, int m);
-int mmult(double *c, double *a, int aRows, int aCols, double *b, int bRows, int bCols);
-void compare_matrix(double *a, double *b, int nRows, int nCols);
-
-/** 
-    Program to multiply a matrix times a matrix using both
     mpi to distribute the computation among nodes and omp
     to distribute the computation among threads.
 */
@@ -64,81 +51,6 @@ int main(int argc, char* argv[])
 	  printf("here2");
   }
   read = getline(&line,&len,inputFileOne);
-  //sscanf(read,"%d,%d",rowOne,colOne);
-  //read = getline(&lineTwo,&lenTwo,inputFileTwo);
-  //sscanf(read,"%d,%d",rowTwo,colTwo);
-  //fileOne = (char*)malloc(rowOne*colOne);
-  //fileTwo = (char*)malloc(rowTwo*colTwo);
-  //fgets(fileOne,(rowOne*colOne),inputFileOne);
- // fgets(fileTwo,(rowTwo*colTwo),inputFileTwo);
-  /*if (argc > 1) {
-    nrows = atoi(argv[1]);
-    ncols = nrows;
-    if (myid == 0) {
-      // Master Code goes here
-      aa = gen_matrix(nrows, ncols);
-      bb = gen_matrix(ncols, nrows);
-      cc1 = malloc(sizeof(double) * nrows * nrows); 
-      starttime = MPI_Wtime();
-      /* Insert your master code here to store the product into cc1 */
-     /* endtime = MPI_Wtime();
-      printf("%f\n",(endtime - starttime));
-      cc2  = malloc(sizeof(double) * nrows * nrows);
-      mmult(cc2, aa, nrows, ncols, bb, ncols, nrows);
-      compare_matrices(cc2, cc1, nrows, nrows);
-    } else {
-      // Slave Code goes here
-    }
-  } else {
-    fprintf(stderr, "Usage matrix_times_vector <size>\n");
-  }
-  MPI_Finalize();
-  return 0;
-  }*/
-	}
-  else{
-	  printf("Imput two file names");
-	}
-}ion among nodes and omp
-    to distribute the computation among threads.
-*/
-
-int main(int argc, char* argv[])
-{
-  if(argc>2){
-  int nrows, ncols;
-  double *aa;	/* the A matrix */
-  double *bb;	/* the B matrix */
-  double *cc1;	/* A x B computed using the omp-mpi code you write */
-  double *cc2;	/* A x B computed using the conventional algorithm */
-  int myid, numprocs;
-  double starttime, endtime;
-  MPI_Status status;
-  /* insert other global variables here */
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-  
-  FILE *inputFileOne;
-  char *fileOne;
-  char *fileTwo;
-  FILE *inputFileTwo;
-  char *line = NULL;
-  char *lineTwo = NULL;
-  size_t len = 0;
-  size_t lenTwo = 0;
-  ssize_t read;
-  int colOne,rowOne,colTwo,rowTwo;
-  
-  if(inputFileOne = fopen(argv[1],"r") != NULL){
-	  //continue;
-	  fprintf("here");
-  }
-  if(inputFileTwo = fopen(argv[2],"r") != NULL){
-	  //continue;
-	  fprintf("here2");
-  }
-  //read = getline(&line,&len,inputFileOne);
   //sscanf(read,"%d,%d",rowOne,colOne);
   //read = getline(&lineTwo,&lenTwo,inputFileTwo);
   //sscanf(read,"%d,%d",rowTwo,colTwo);
